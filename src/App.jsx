5ab136aee1457d6a85ae25f2fd6ec1b8785c960e
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-08-02 21:45:15 -0400

    app begins

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,50 @@ import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
 import Corn from './components/Corn';
 import './App.scss';
-// import model from './assets/model2.glb';
-// import envMap from './assets/modern_buildings_2k.hdr';
 
 const App = () => {
-  const cameraRef = useRef();
-  const controlsRef = useRef();
   const canvasRef = useRef();
+  const [kernals, setKernals] = useState([]);
+  const [currentKernal, setCurrentKernal] = useState({ x: 0, y: 0 });
 
+  const handleKeydown = ({ key }) => {
+    switch(key) {
+      case 'ArrowLeft':
+        setCurrentKernal((prevKernal) => ({ ...prevKernal, x: prevKernal.x - 1 }));
+      break;
+      case 'ArrowRight':
+        setCurrentKernal((prevKernal) => ({ ...prevKernal, x: prevKernal.x + 1 }));
+      break;
+      case 'ArrowUp':
+        setCurrentKernal((prevKernal) => ({ ...prevKernal, y: prevKernal.y - 1 }));
+      break;
+      case 'ArrowDown':
+        setCurrentKernal((prevKernal) => ({ ...prevKernal, y: prevKernal.y + 1 }));
+      break;
+    }
+  };
+
+  useEffect(() => {
+    const cols = 10;
+    const rows = 10;
+    const _kernals = [];
+    let count = 0;
+    for(let col = 0; col < cols; col+=1) {
+      for(let row = 0; row < rows; row += 1) {
+        count+= 1;
+        _kernals.push({id: count, x: col, y: row, status: 'yellow'});
+      }
+    }
+    setKernals(_kernals);
+    addEventListener('keydown', handleKeydown);
+    return () => { removeEventListener('keydown', handleKeydown); };
+  }, []);
 
   return (
     <div className="app">
       <Canvas ref={canvasRef}>
         <OrbitControls />
-        <Corn />
+        <Corn kernals={kernals} currentKernal={currentKernal} />
         <pointLight position={[0, 10, 10]} />
       </Canvas>
 
--- src/components/Corn.jsx
+++ src/components/Corn.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Sphere } from '@react-three/drei';
 import { MeshStandardMaterial } from 'three';
 
-const Corn = () => {
+const Corn = (props) => {
+  const { currentKernal, kernals } = props;
+  console.log(kernals);
+  // const [kernals, setKernals] = useState([])
   const rows = 20;
   const cols = 20;
-  const spacing = 1.5; // Adjust this value to set the distance between spheres
+  const spacing = 0.5; // Adjust this value to set the distance between spheres
 
   // Function to calculate the position of each sphere
   const calculatePosition = (row, col) => {
@@ -14,25 +17,42 @@ const Corn = () => {
     return [x, y, 0];
   };
 
-  const cornMat = new MeshStandardMaterial({ 
+  const cornMat = new MeshStandardMaterial({
     color: 0xCCCC00,
-    roughness: .5,
-    metalness: .01,
-  })
+    roughness: 0.5,
+    metalness: 0.01,
+  });
+
+  const selectedCornMat = new MeshStandardMaterial({
+    color: 0xFFCC00,
+    roughness: 0.2,
+    metalness: 0.05,
+    emissiveIntensity: 0.5,
+  });
 
   return (
     <group>
-      {Array.from({ length: rows }).map((_, row) => Array.from({ length: cols }).map((_, col) => {
+      {
+        kernals.map(kernal => (
+          <Sphere
+            key={kernal.id}
+            args={[0.5, 32, 32]}
+            position={[kernal.x, kernal.y, 0]}
+            material={cornMat}
+          />
+        ))
+      }
+      {/* {Array.from({ length: rows }).map((_, row) => Array.from({ length: cols }).map((_, col) => {
         const [x, y, z] = calculatePosition(row, col);
         return (
           <Sphere
             key={`sphere-${row}-${col}`}
-            args={[0.9, 16, 16]}
+            args={[0.5, 32, 32]}
             position={[x, y, z]}
             material={cornMat}
           />
         );
-      }),)}
+      }),)} */}
     </group>
   );
 };
