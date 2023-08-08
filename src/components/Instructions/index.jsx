6c1b933675715bb96e249811389f99df14ac8457
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-08-08 08:04:23 -0400

    finish sharing

--- src/components/Instructions/index.jsx
+++ src/components/Instructions/index.jsx
@@ -8,12 +8,12 @@ const Instructions = (props) => {
   // console.log('instructions page');
   return (
     <div className="instructions">
-      <div className="instructions-content">
+      <div className="instructions-content modal">
         <h1 className="instructions-content-title">- MAIZ- </h1>
         <p>Use the arrow keys to make your way to the right end of the cob and escape the maiz!</p>
         <p>In regular mode, you can only move to yellow kernals, brown kernals are walls.</p>
         {/* <div className="options"> */}
-        {/* <h3>Options</h3> */}
+        <h3>Options</h3>
         {/* <div className="option">
           <p>Mode:</p>
           <label for="normal">normal</label>
@@ -21,6 +21,21 @@ const Instructions = (props) => {
           <label for="normal">free</label>
           <input type="radio" name="mode" value="free" />
         </div> */}
+        <div className="option">
+          <p>Difficulty:</p>
+          <label>
+            Easy
+            <input type="radio" name="mode" value="easy" />
+          </label>
+          <label>
+            Medium
+            <input type="radio" name="mode" value="medium" />
+          </label>
+          <label>
+            Hard
+            <input type="radio" name="mode" value="hard" />
+          </label>
+        </div>
         <img className="graphic" src={cornImage} alt="corn" />
         <button
           type="button"
--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import './index.scss';
-import { EmailShareButton, FacebookShareButton, LinkedinShareButton, RedditShareButton, TwitterShareButton } from 'react-share';
+import {
+  EmailShareButton, EmailIcon,
+  FacebookShareButton, FacebookIcon,
+  LinkedinShareButton, LinkedinIcon,
+  RedditShareButton, RedditIcon,
+  TwitterShareButton, TwitterIcon,
+} from 'react-share';
 import { messages } from '../../assets/content.json';
+
+const random = array => array[Math.floor(Math.random() * array.length)];
 
 const Results = (props) => {
   const { result } = props;
-  // console.log('results');
   const index = Math.floor(Math.random() * messages[result].length);
   const message = messages[result][index];
 
+  const url = 'https://daveseidman.gitlab.io/cornmaze';
+
   return (
     <div className="results">
-      <div className="results-content">
+      <div className="results-content modal">
         <h2>{message}</h2>
-        {/* <a href="share">Share Your Results!</a> */}
-        <TwitterShareButton
-          url="https://daveseidman.gitlab.io/cornmaze"
-          title="try your luck!"
-          caption="a corn maze"
-          hashtags={['webgl', 'corn']}
-        >share me
-        </TwitterShareButton>
+        <div className="results-content-share">
+          <h3>Now go and brag about it!</h3>
+          <TwitterShareButton
+            url={url}
+            title={random(messages.share.titles)}
+            hashtags={messages.share.hashtags}
+          ><TwitterIcon />
+          </TwitterShareButton>
+          <FacebookShareButton
+            url={url}
+            quote="test quote"
+            hashtag="#webgl,#threejs,#reactthreefiber"
+          ><FacebookIcon />
+          </FacebookShareButton>
+          <LinkedinShareButton
+            url={url}
+            title={random(messages.share.titles)}
+            summary="test summary"
+            source={url}
+          >
+            <LinkedinIcon />
+          </LinkedinShareButton>
+          <RedditShareButton
+            url={url}
+            title={random(messages.share.titles)}
+          ><RedditIcon />
+          </RedditShareButton>
+          <EmailShareButton
+            url={url}
+            subject={random(messages.share.titles)}
+          ><EmailIcon />
+          </EmailShareButton>
+        </div>
       </div>
     </div>
   );
